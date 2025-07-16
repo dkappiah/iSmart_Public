68c2fcfb43643539353f48084b12edb03490d235
SKArABS <skabsam@gmail.com>
2025-07-16 15:35:16 +0000

    Axios setup

---
+++ digital-wallet/resources/js/react-app/src/api.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const apiClient = axios.create({
+    baseURL: 'http://digital-wallet.test/api', 
+    withCredentials: true, 
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+    },
+});
+
+
+export const getCsrfToken = async () => {
+    try {
+        await apiClient.get('/sanctum/csrf-cookie'); 
+    } catch (error) {
+        console.error("Failed to get CSRF cookie:", error);
+        throw error; 
+    }
+};
+
+export default apiClient;
