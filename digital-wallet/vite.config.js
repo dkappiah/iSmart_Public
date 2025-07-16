d781d59cb9c3676cdde2bba727746a8554e3516f
SKArABS <skabsam@gmail.com>
2025-07-16 19:52:11 +0000

    Resolve vite integration for React app(formally required starting the dev server from react-app. Now runs from root )

--- digital-wallet/vite.config.js
+++ digital-wallet/vite.config.js
@@ -1,11 +1,25 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react'; // <--- Make sure this line is present
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            
+            input: 'resources/js/react-app/src/main.jsx',
+            refresh: true, 
         }),
+        react(), 
     ],
+    
+    server: {
+        hmr: {
+            host: 'digital-wallet.test', 
+            protocol: 'ws', 
+        },
+        host: true, 
+        watch: {
+            usePolling: true, 
+        },
+    },
 });
